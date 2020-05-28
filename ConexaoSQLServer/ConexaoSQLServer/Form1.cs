92953d904510b07550a2eafe3481b819c748f7f5
maxwell.miquelino <maxwell.miquelino@gmail.com>
2020-05-28 09:27:14 -0300

    Conexão SQL Server Nativo
    Como utilizar uma conexão SQL Server Nativo em CSharp

---
+++ ConexaoSQLServer/ConexaoSQLServer/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConexaoSQLServer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection cnx = new SqlConnection())
+                {
+                    cnx.ConnectionString = "data source=MAXWELL;initial catalog=master; user id=sa; password=1234";
+                    cnx.Open();
+                    label1.Text = "Conexão com SQL Server com sucesso !!!";
+                }
+            }
+            catch(SqlException erro)
+            {
+                label1.Text = erro.Message;
+            }
+        }
+    }
+}
